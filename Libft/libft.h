291cfc556a3038d6967875d5591a8464a9dfd3da
AndranikAdyan <andranikadyan@gmail.com>
2025-01-16 16:17:24 +0400

    add libft

---
+++ Libft/libft.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 19:55:12 by aadyan            #+#    #+#             */
+/*   Updated: 2025/01/13 17:16:23 by aadyan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(int ch);
+int		ft_isalpha(int ch);
+int		ft_isascii(int ch);
+int		ft_isdigit(int ch);
+int		ft_isprint(int ch);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *ptr, int value, size_t num);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strnstr(const char *str, const char *to_find, size_t n);
+int		ft_strncmp(char *s1, char *s2, unsigned int n);
+char	*ft_strdup(char *src);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *str, int ch);
+char	*ft_strrchr(const char *s, int c);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(const char *nptr);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int nb);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putstr_fd(char *s, int fd);
+
+#endif
---
+++ Libft/src/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 17:05:38 by aadyan            #+#    #+#             */
+/*   Updated: 2024/10/27 17:30:48 by aadyan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	unsigned int	i;
+	unsigned int	dst_len;
+
+	if (size == 0)
+		return (ft_strlen(src));
+	if (size <= (size_t)ft_strlen(dst))
+		return (size + ft_strlen(src));
+	dst_len = ft_strlen(dst);
+	i = 0;
+	while (src[i] && i < size - dst_len - 1)
+	{
+		dst[dst_len + i] = src[i];
+		++i;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + ft_strlen(src));
+}
